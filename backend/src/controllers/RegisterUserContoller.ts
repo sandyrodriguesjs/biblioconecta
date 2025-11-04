88c2ae5d2b22e52a078ece65426bd263a0743c58
sandyrodriguesjs <sandyrodriguesdev@gmail.com>
2025-11-03 21:54:05 -0300

    fix: removendo retorno de senha

--- backend/src/controllers/RegisterUserContoller.ts
+++ backend/src/controllers/RegisterUserContoller.ts
@@ -5,13 +5,18 @@ class RegisterUserController {
   public async handle(req: Request, res: Response): Promise<Response> {
     try {
       const novoUsuario = await registerUserService(req.body);
+
+      //Remove o campo de senha antes de enviar a resposta
+      const { password, ...usuarioSemSenha } = novoUsuario;
+
       return res.status(201).json({
         mensagem: "Usuário criado com sucesso!",
-        usuario: novoUsuario,
+        usuario: usuarioSemSenha,
       });
-    } catch (error) {
+    } catch (error: any) {
+      console.error("Erro ao registrar usuário:", error);
       return res.status(400).json({
-        erro: error instanceof Error ? error.message : error,
+        erro: error instanceof Error ? error.message : "Erro desconhecido",
       });
     }
   }
