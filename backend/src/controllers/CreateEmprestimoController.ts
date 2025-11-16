63d9fb6e23b42ec623d80195a97553ab77f19c00
sandyrodriguesjs <sandyrodriguesdev@gmail.com>
2025-11-15 22:23:16 -0300

    fix: implementando tratamentos de tipagens dos campos enviados na requisição

--- backend/src/controllers/CreateEmprestimoController.ts
+++ backend/src/controllers/CreateEmprestimoController.ts
@@ -4,10 +4,31 @@ import { CreateEmprestimoService } from "../services/CreateEmprestimoService";
 export class CreateEmprestimoController {
   async handle(req: Request, res: Response) {
     try {
-      const { userId, exemplarId } = req.body;
+      // Verifica se o admin está logado
+      // const userRole = (req as any).role;
+
+      // if (userRole !== "ADMIN") {
+      //   return res.status(403).json({ error: "Apenas administradores podem registrar empréstimos." });
+      // }
+
+      let { userId, exemplarId } = req.body;
+
+      if (!userId || !exemplarId) {
+        return res.status(400).json({ error: "Campos userId e exemplarId são obrigatórios." });
+      }
+
+      userId = Number(userId);
+      exemplarId = Number(exemplarId);
+
+      if (isNaN(userId) || isNaN(exemplarId)) {
+        return res.status(400).json({ error: "userId e exemplarId devem ser números válidos." });
+      }
+
       const service = new CreateEmprestimoService();
       const result = await service.execute({ userId, exemplarId });
+
       return res.status(201).json(result);
+
     } catch (err: any) {
       return res.status(400).json({ error: err.message });
     }
