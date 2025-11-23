3d38363ecc15cb464ddd9812cca62178a518940f
sandyrodriguesjs <sandyrodriguesdev@gmail.com>
2025-11-23 16:28:24 -0300

    feat: implementando logica para exibir determinados atalhos apenas para Admins

--- frontend/web/biblioconecta/src/app/components/sideBar.tsx
+++ frontend/web/biblioconecta/src/app/components/sideBar.tsx
@@ -1,8 +1,30 @@
 "use client";
-import { Home, BookCheck, LogOut, LibraryBig } from "lucide-react";
+
+import { Home, BookCheck, LogOut, LibraryBig, UserRoundCog } from "lucide-react";
 import Link from "next/link";
+import { jwtDecode } from "jwt-decode";
+import { useEffect, useState } from "react";
+
+interface DecodedToken {
+  role: string;
+}
 
 export default function SideBar() {
+  const [role, setRole] = useState<string | null>(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      try {
+        const decoded = jwtDecode<DecodedToken>(token);
+        setRole(decoded.role);
+      } catch (err) {
+        console.error("Erro ao decodificar token:", err);
+      }
+    }
+  }, []);
+
   return (
     <aside className="fixed top-0 left-0 w-56 bg-[#1e1e2f] text-white h-screen flex flex-col justify-between">
       <div>
@@ -14,12 +36,22 @@ export default function SideBar() {
           <Link href="/homePage" className="flex items-center gap-3 px-6 py-3 hover:bg-[#2a2a3d] transition">
             <Home size={20} /> Home
           </Link>
-          <Link href="/listBook" className="flex items-center gap-3 px-6 py-3 hover:bg-[#2a2a3d] transition">
-            <LibraryBig size={20} /> Acervo de Livros
-          </Link>
-          <Link href="/pendingReservations" className="flex items-center gap-3 px-6 py-3 hover:bg-[#2a2a3d] transition">
-            <BookCheck size={20} /> Reservas pendentes
-          </Link>
+
+          {role === "ADMIN" && (
+            <>
+              <Link href="/listBook" className="flex items-center gap-3 px-6 py-3 hover:bg-[#2a2a3d] transition">
+                <LibraryBig size={20} /> Acervo de Livros
+              </Link>
+
+              <Link href="/pendingReservations" className="flex items-center gap-3 px-6 py-3 hover:bg-[#2a2a3d] transition">
+                <BookCheck size={20} /> Reservas pendentes
+              </Link>
+
+              <Link href="/adminUsers" className="flex items-center gap-3 px-6 py-3 hover:bg-[#2a2a3d] transition">
+                <UserRoundCog size={20} /> Gerenciar Usuários
+              </Link>
+            </>
+          )}
         </nav>
       </div>
 
