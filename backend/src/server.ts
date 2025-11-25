eeea79e29dbf3a9f62e2f2216a0f9cbc63074c18
sandyrodriguesjs <sandyrodriguesdev@gmail.com>
2025-11-25 20:41:45 -0300

    feat: implementando configurações de CORS para ambiente em produção

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,11 +9,25 @@ import emprestimosRoutes from "./routes/emprestimosRoutes";
 const app = express();
 const prisma = new PrismaClient();
 
-// ✅ Configuração de CORS
+const allowedOrigins = [
+  "http://localhost:3000",               // Ambiente de desenvolvimento
+  "https://biblioconecta.vercel.app",   // Produção (Vercel)
+];
+
+//CORS dinâmico
 app.use(
   cors({
-    origin: "http://localhost:3000", // URL do seu frontend
-    credentials: true,          
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      console.log("❌ Origem bloqueada pelo CORS:", origin);
+      return callback(new Error("CORS blocked"), false);
+    },
+    credentials: true,
   })
 );
 
@@ -26,4 +40,6 @@ app.use("/", booksCirculationRoutes);
 app.use("/", emprestimosRoutes);
 
 const PORT = process.env.PORT || 3333;
-app.listen(PORT, () => console.log(`🚀 Servidor rodando na porta ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`🚀 Servidor rodando na porta ${PORT}`)
+);
