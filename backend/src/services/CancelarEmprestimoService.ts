b1758bbb2221d40629a534c0ed9709f6a7e0ecf7
sandyrodriguesjs <sandyrodriguesdev@gmail.com>
2025-11-25 21:06:03 -0300

    removendo arquivo desnecessario

--- backend/src/services/CancelarEmprestimoService.ts
+++
@@ -1,8 +0,0 @@
-import { CancelarEmprestimoRepository } from "../repositories/CancelarEmprestimoRepository";
-
-export class CancelarEmprestimoService {
-  async execute(id: number) {
-    const repo = new CancelarEmprestimoRepository();
-    return await repo.cancelar(id);
-  }
-}
